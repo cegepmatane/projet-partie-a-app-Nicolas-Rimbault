fe751fbb339d56f3e31b8c09c6abf823565f4169
Nicolas-Rimbault <54377194+Nicolas-Rimbault@users.noreply.github.com>
2021-04-15 19:44:47 +0200

    ajout single plage application modifier

---
+++ Application.js
@@ -0,0 +1,41 @@
+﻿class Application {
+  constructor(window, vueListeSwitch, vueSwitch){
+
+    this.window = window;
+    this.listeSwitch = [{couleur:"Bleu", prix:"0,71", distanceActivation:"2,2mm", distanceMorte:"1,8mm", distanceTotal:"4mm", type:"tactil", forceAction:"50g", marque:"HyperX",clickyOrNot:"clicky", id:0},
+                        {couleur:"Rouge", prix:"0,71", distanceActivation:"2mm", distanceMorte:"2mm", distanceTotal:"4mm", type:"lineaire", forceAction:"45g", marque:"HyperX",clickyOrNot:"Not", id:1},
+                        {couleur:"Noir", prix:"0,71", distanceActivation:"2mm", distanceMorte:"2mm", distanceTotal:"4mm", type:"lineaire", forceAction:"60g", marque:"Cherry MX",clickyOrNot:"Not", id:2},]
+                        //{couleur:"", prix:"", distanceActivation:"", distanceMorte:"", distanceTotal:"", type:"", forceAction:"", marque:"",clickyOrNot:"", id:0},
+    this.vueListeSwitch = vueListeSwitch;
+
+    this.vueSwitch = vueSwitch;
+
+    // C'est l'équivalent de function(){this.naviguer()}
+    this.window.addEventListener("hashchange", () =>this.naviguer());
+
+    this.naviguer();
+  }
+
+  naviguer(){
+    let hash = window.location.hash;
+
+    if(!hash){
+
+      this.vueListeSwitch.initialiserListeSwitch(this.listeSwitch);
+      this.vueListeSwitch.afficher();
+
+    }else{
+
+      let navigation = hash.match(/^#switch\/([0-9]+)/);
+      let idSwitch = navigation[1];
+
+      let switch_ = this.listeSwitch.find(switch_ => switch_.id === parseInt(idSwitch));
+      this.vueSwitch.initialiserSwitch(switch_);
+      this.vueSwitch.afficher();
+    }
+  }
+
+}
+
+new Application(window, new VueListeSwitch(), new VueSwitch());
+
---
+++ VueListeSwitch.js
@@ -0,0 +1,28 @@
+﻿class VueListeSwitch{
+  constructor(){
+    this.html = document.getElementById("html-vue-liste-switch").innerHTML;
+    this.listeSwitchDonnee = null;
+  }
+
+  initialiserListeSwitch(listeSwitchDonnee){
+    this.listeSwitchDonnee = listeSwitchDonnee;
+  }
+
+  afficher(){
+    document.getElementsByTagName("body")[0].innerHTML = this.html;
+
+    let listeSwitch = document.getElementById("liste-switch");
+    const listeSwitchItemHTML = listeSwitch.innerHTML;
+    let listeSwitchHTMLRemplacement = "";
+
+    for(var numeroSwitch in this.listeSwitchDonnee){
+      let listeSwitchItemHTMLRemplacement = listeSwitchItemHTML;
+      listeSwitchItemHTMLRemplacement = listeSwitchItemHTMLRemplacement.replace("{Switch.id}",this.listeSwitchDonnee[numeroSwitch].id);
+      listeSwitchItemHTMLRemplacement = listeSwitchItemHTMLRemplacement.replace("{Switch.couleur}",this.listeSwitchDonnee[numeroSwitch].couleur);
+      listeSwitchHTMLRemplacement += listeSwitchItemHTMLRemplacement;
+    }
+
+    listeSwitch.innerHTML = listeSwitchHTMLRemplacement;
+  }
+
+}
