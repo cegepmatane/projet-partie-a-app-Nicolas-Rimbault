b470b1f9c39a12c0d1ae846828b4268b87523b8f
Nicolas-Rimbault <54377194+Nicolas-Rimbault@users.noreply.github.com>
2021-04-16 17:12:58 +0200

    modification SwitchDAO et Application pour DAO

--- Application.js
+++ Application.js
@@ -1,19 +1,13 @@
 ﻿class Application {
-  constructor(window, vueListeSwitch, vueSwitch){
+  constructor(window, vueListeSwitch, vueSwitch, switchDAO){
 
     this.window = window;
-    /*
-    this.listeSwitch = [{couleur:"Bleu", prix:"0,71", distanceActivation:"2,2mm", distanceMorte:"1,8mm", distanceTotal:"4mm", type:"tactil", forceAction:"50g", marque:"HyperX",clickyOrNot:"clicky", rgbCompatibility:"Oui", dureeDeVie:"80 million", id:0},
-                        {couleur:"Rouge", prix:"0,71", distanceActivation:"2mm", distanceMorte:"2mm", distanceTotal:"4mm", type:"lineaire", forceAction:"45g", marque:"HyperX",clickyOrNot:"Not", rgbCompatibility:"Oui", dureeDeVie:"80 million", id:1},
-                        {couleur:"Noir", prix:"0,71", distanceActivation:"2mm", distanceMorte:"2mm", distanceTotal:"4mm", type:"lineaire", forceAction:"60g", marque:"Cherry MX",clickyOrNot:"Not", rgbCompatibility:"Oui", dureeDeVie:"100 million", id:2},]
-                        //{couleur:"", prix:"", distanceActivation:"", distanceMorte:"", distanceTotal:"", type:"", forceAction:"", marque:"",clickyOrNot:"", rgbCompatibility:"", dureeDeVie:"", id:0}
-    */
-    this.listeSwitch = [new Switch("Bleu", "0,71", "2,2mm", "1,8mm", "4mm", "tactil", "50g", "HyperX", "clicky", "Oui", "80 million", 0),
-                        new Switch("Rouge", "0,71", "2mm", "2mm", "4mm", "lineaire", "45g", "HyperX", "Not", "Oui", "80 million", 1),
-                        new Switch("Noir", "0,71", "2mm", "2mm", "4mm", "lineaire", "60g", "Cherry", "Not", "Oui", "100 million", 2)]
+
     this.vueListeSwitch = vueListeSwitch;
 
     this.vueSwitch = vueSwitch;
+
+    this.switchDAO = switchDAO;
 
     // C'est l'équivalent de function(){this.naviguer()}
     this.window.addEventListener("hashchange", () =>this.naviguer());
@@ -26,7 +20,7 @@
 
     if(!hash){
 
-      this.vueListeSwitch.initialiserListeSwitch(this.listeSwitch);
+      this.vueListeSwitch.initialiserListeSwitch(this.switchDAO.lister());
       this.vueListeSwitch.afficher();
 
     }else{
@@ -34,7 +28,7 @@
       let navigation = hash.match(/^#switch\/([0-9]+)/);
       let idSwitch = navigation[1];
 
-      let switch_ = this.listeSwitch.find(switch_ => switch_.id === parseInt(idSwitch));
+      let switch_ = this.switchDAO.chercher(parseInt(idSwitch));
       this.vueSwitch.initialiserSwitch(switch_);
       this.vueSwitch.afficher();
     }
@@ -42,5 +36,5 @@
 
 }
 
-new Application(window, new VueListeSwitch(), new VueSwitch());
+new Application(window, new VueListeSwitch(), new VueSwitch(), new SwitchDAO());
 
--- SwitchDAO.js
+++ SwitchDAO.js
@@ -1,44 +1,15 @@
 class SwitchDAO{
     constructor(){
-        this.listeSwitchMemoire = [new Switch("Bleu", "0,71", "2,2mm", "1,8mm", "4mm", "tactil", "50g", "HyperX", "clicky", "Oui", "80 million", 0),
+        this.listeSwitch = [new Switch("Bleu", "0,71", "2,2mm", "1,8mm", "4mm", "tactil", "50g", "HyperX", "clicky", "Oui", "80 million", 0),
                             new Switch("Rouge", "0,71", "2mm", "2mm", "4mm", "lineaire", "45g", "HyperX", "Not", "Oui", "80 million", 1),
                             new Switch("Noir", "0,71", "2mm", "2mm", "4mm", "lineaire", "60g", "Cherry", "Not", "Oui", "100 million", 2)];
+    }
     
-        localStorage['switch'] = JSON.stringify(listeSwitchMemoire);
-        this.listeSwitch = [];
-      }
-    
-      lister(){
-        this.listeSwitch = [];
-        let listeSwitchMemoire = [];
-        if(localStorage['switch']){
-          listeSwitchMemoire = JSON.parse(localStorage['switch']);
-        }
-    
-        for(let position in listeSwitchMemoire){
-          let switchs = new Switch(listeSwitchMemoire[position].couleur,
-                                  listeSwitchMemoire[position].prix,
-                                  listeSwitchMemoire[position].distanceActivation,
-                                  listeSwitchMemoire[position].distanceMorte,
-                                  listeSwitchMemoire[position].distanceTotal,
-                                  listeSwitchMemoire[position].type,
-                                  listeSwitchMemoire[position].forceAction,
-                                  listeSwitchMemoire[position].marque,
-                                  listeSwitchMemoire[position].clickyOrNot,
-                                  listeSwitchMemoire[position].rgbCompatibility,
-                                  listeSwitchMemoire[position].dureeDeVie,
-                                  listeSwitchMemoire[position].id);
-    
-          this.listeSwitch.push(switchs);
-        }
-    
+    lister(){
         return this.listeSwitch;
-    
-      }
+    }
     
       chercher(id){
-        this.lister();
-        return this.listeSwitch.find(switchs => switchs.id === id);
+        return this.listeSwitch.find(switch_ => switch_.id === id);
       }
-    
     }
